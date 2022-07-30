afccf2a410641edfe9b89782c060dc72438985ac
박형주 <hyeongju1122@gmail.com>
2022-07-30 19:30:37 +0900

    preventDefault()

--- App/app.js
+++ App/app.js
@@ -1,10 +1,22 @@
 const loginForm = document.getElementById('login-form');
 const loginInput = loginForm.querySelector("input");
+const link = document.querySelector('a');
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    // greeting.innerText = "Hello " + username + "!";
+    greeting.innerText = `Hello ${username}!`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+function handleLinkClick(event){
+    event.preventDefault();
     console.log(event);
-    console.log(loginInput.value);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
