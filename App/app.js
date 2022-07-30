cb55703c46f4a56ff20504190c5cda366c6dfd93
박형주 <hyeongju1122@gmail.com>
2022-07-30 20:15:47 +0900

    Today's JS Study Finished

--- App/app.js
+++ App/app.js
@@ -2,15 +2,16 @@ const loginForm = document.getElementById('login-form');
 const loginInput = loginForm.querySelector("input");
 const link = document.querySelector('a');
 const greeting = document.querySelector("#greeting");
+// String 딱 한번만 작성하기 위해!
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    // greeting.innerText = "Hello " + username + "!";
-    greeting.innerText = `Hello ${username}!`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
 function handleLinkClick(event){
@@ -20,3 +21,17 @@ function handleLinkClick(event){
 
 loginForm.addEventListener("submit", onLoginSubmit);
 link.addEventListener("click", handleLinkClick);
+
+const savedUsername = localStorage.getItem("username");
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}!`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+   paintGreetings(savedUsername);
+}
