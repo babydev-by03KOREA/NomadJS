6ee0dc5b6e51eda1ec3d0025813980f4037c8129
박형주 <hyeongju1122@gmail.com>
2022-07-30 18:39:11 +0900

    두번째 강의

--- App/app.js
+++ App/app.js
@@ -1,16 +1,10 @@
 const loginForm = document.getElementById('login-form');
 const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
 
-function LoginBtnClick() {
-    // const username = loginInput.value;
-    // if (username === ""){
-    //     alert("아이디를 입력하세요!");
-    // } else if (username.length > 15){
-    //     alert("아이디는 15자를 초과할 수 없어요ㅠㅠ")
-    // } else {
-    //     alert(username + " 님 환영합니다!");
-    // }
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(event);
+    console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", LoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
